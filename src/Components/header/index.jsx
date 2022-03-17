43d6bd78421d1ed416f79e308ba19ca53edbc864
wesley <wesleykaihara@gmail.com>
2022-03-17 15:10:56 -0300

    :tada: Responsividade Inicial

--- src/Components/header/index.jsx
+++ src/Components/header/index.jsx
@@ -1,15 +1,29 @@
 import './style.css';
 
 import PlaystationLogo from '../../images/Logo.png'
+import { useState } from 'react';
 
 export default function Header() {
+
+  const [mobile, setMobile] = useState("");
+
+  function MobileMenu() {
+    if (mobile === "mostrar") {
+      setMobile("");
+    } else {
+      setMobile("mostrar");
+    }
+    return;
+  }
+
   return (
     <header>
       <div className="sony"><h1>Sony</h1></div>
-      <div className="menu">
+      <div className={`menu ${mobile}`}>
         <div className="menu_links">
-          <img src={PlaystationLogo} alt="PlaystationLogo" className='Logo_Ps' />
-          <ul className='menu_links_content'>
+          <a href="/"><img src={PlaystationLogo} alt="PlaystationLogo" className='Logo_Ps' /></a>
+          <img src={PlaystationLogo} alt="PlaystationLogo" onClick={MobileMenu} className='playstationMobileLogo' />
+          <ul className={`menu_links_content ${mobile}`}>
             <li>Jogos</li>
             <li>Hardware</li>
             <li>Serviços</li>
@@ -19,7 +33,7 @@ export default function Header() {
           </ul>
         </div>
 
-        <div className="header_login">
+        <div className={`header_login ${mobile}`}>
           <button>Inciar sessão</button>
         </div>
       </div>
--- src/Components/produtos/index.jsx
+++ src/Components/produtos/index.jsx
@@ -9,6 +9,11 @@ import Camera from '../../images/camera.jpg';
 
 
 export default function Produtos() {
+
+  function SelecionarProduto(item) {
+    console.log(item)
+  }
+
   return (
     <div>
       <div className="produtos_content">
@@ -21,23 +26,23 @@ export default function Produtos() {
       </div>
 
       <div className="produtos_selector">
-        <div className="produtos_item selecionado">
+        <div id='1' className="produtos_item selecionado" onClick={(item) => SelecionarProduto(item)}>
           <img src={PS5} alt="ps5" />
           <p>Console PS5</p>
         </div>
-        <div className="produtos_item ">
+        <div id='2' className="produtos_item ">
           <img src={DualSense} alt="ps5" />
           <p>Controle sem fio DualSense</p>
         </div>
-        <div className="produtos_item">
+        <div id='3' className="produtos_item">
           <img src={Headset} alt="ps5" />
           <p>Headset sem fio PULSE 3D™</p>
         </div>
-        <div className="produtos_item">
+        <div id='4' className="produtos_item">
           <img src={ControleMidea} alt="ps5" />
           <p>Console de mídia</p>
         </div>
-        <div className="produtos_item">
+        <div id='5' className="produtos_item">
           <img src={Camera} alt="Camera" />
           <p>Câmera HD</p>
         </div>
